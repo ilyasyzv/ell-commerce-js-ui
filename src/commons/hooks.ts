c99ff8bf9fdf0d3cdd62ec84602f6a363b05edeb
Dmitriy <dvpilipenko86@gmail.com>
2023-02-28 12:43:37 +0600

    EEU-483 Add useBreakpoints

---
+++ src/commons/hooks.ts
@@ -0,0 +1,29 @@
+import {MutableRefObject, useEffect, useMemo, useState} from "react";
+
+export const useBreakpoints = <T,> (ref: MutableRefObject<any>, breakpoints: T[]) => {
+    const [breakpoint, setBreakPoint] = useState<T>()
+    const sortedBreakPoints = useMemo(() => [...breakpoints].sort().reverse(), [breakpoints])
+
+    const observer = useMemo(() => new ResizeObserver((args) => {
+        const width = Math.round(args[0].contentRect.width)
+        for (let [index, item] of sortedBreakPoints.entries()) {
+            if (item < width  || index === sortedBreakPoints.length - 1) {
+                setBreakPoint(item)
+                break
+            }
+        }
+
+    }),[ref.current])
+    useEffect(()=> {
+        if (ref.current){
+            observer.observe(ref.current)
+        }
+        return ()=> {
+            if (ref.current) {
+                observer.unobserve(ref.current)
+            }
+        }
+    }, [ref.current])
+
+    return breakpoint
+}
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,60 @@
+import React, {useLayoutEffect} from "react";
+import {MutableRefObject, useEffect, useMemo, useRef, useState} from "react";
+import styled from "styled-components";
+import {useBreakpoints} from "../../commons/hooks";
+
+enum EnumStyledDivBreakPoints {
+    small = 200,
+    mdeium = 500,
+    large = 700
+}
+
+interface StyledDivProps {
+    readonly breakpoint: EnumStyledDivBreakPoints | undefined
+}
+
+const StyledChildDiv = styled.div`
+  width: 150px;
+  height: 150px;
+  background-color: sienna;
+
+`
+
+const StyledDiv = styled.div<StyledDivProps>`
+    width: 100%;
+    height: 200px;
+    background-color: gold;
+  
+    ${props => {
+          switch (props.breakpoint) {
+            case EnumStyledDivBreakPoints.small:
+              return `background-color: green;`
+            case EnumStyledDivBreakPoints.mdeium:
+              return `background-color: red;`
+            case EnumStyledDivBreakPoints.large:
+              return `background-color: yellow;`
+          }
+        }
+    }
+
+    .child-div {
+      ${props => {
+        switch (props.breakpoint) {
+          case EnumStyledDivBreakPoints.small:
+            return `background-color: black;`
+        }
+      }}
+    }`
+
+export const Container = () => {
+    const ref = useRef(null)
+    const breakpoint = useBreakpoints<EnumStyledDivBreakPoints>(ref, [
+            EnumStyledDivBreakPoints.small,
+            EnumStyledDivBreakPoints.mdeium,
+            EnumStyledDivBreakPoints.large
+    ])
+    console.log(breakpoint, "BreakPoint")
+    return <StyledDiv ref={ref} breakpoint={breakpoint}>
+        <StyledChildDiv className={"child-div"}></StyledChildDiv>
+    </StyledDiv>
+}
