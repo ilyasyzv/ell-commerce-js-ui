1e42b75fad6cfcb89544e2b81dd60b24abf4ce0a
Fernando Meira <fernando.meira@pearson.com>
2023-02-28 19:46:39 -0800

    Added RecommendedBundle component to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,11 @@
-import "../localizations"
+import "../localizations";
 export * from "./Catalog";
 export * from "./CatalogItem";
 export * from "./ShoppingCartIcon";
 export * from "./CartQty";
 export * from "./CartOrderSummary";
 export * from "./EmptyCart";
-export * from "./CartProduct"
-export * from "./CartProductHeader"
-export * from "./HTMLCartProduct"
+export * from "./CartProduct";
+export * from "./CartProductHeader";
+export * from "./HTMLCartProduct";
+export * from "./RecommendedBundle";
